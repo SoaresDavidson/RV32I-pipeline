d2f7e047f93847818999f8515d0fcf65c5b36b20
João Pedro Saleh de Sousa <161906672+AthomicalGD@users.noreply.github.com>
2025-10-06 17:11:08 -0300

    Program Counter

---
+++ verilog_code/PC.v
@@ -0,0 +1,25 @@
+module ProgramCounter (
+  input wire Clk,
+  input wire Reset,
+  input wire Control,
+  input wire Enable,
+  input wire [31:0] Target,
+  output reg [31:0] pc
+);
+  
+  always @(posedge Clk or Reset) begin
+    if (Reset == 1) begin
+      pc <= 32'b0;
+    end
+    else if (Enable ==1) begin
+      if (Control == 1) begin
+        pc <= Target;
+      end
+      else begin
+        pc <= pc + 4;
+      end
+    end
+  end
+endmodule
+    
+      
